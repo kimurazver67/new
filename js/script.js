2dab94982f3f47bcabe5c5a9fb0062139de901c0
Nikita Zverev <692623ak@gmail.com>
2023-01-25 13:56:45 +1000

    add responsive

--- js/script.js
+++ js/script.js
@@ -12,3 +12,21 @@ search.addEventListener('click', ()=>{
 })
 
 
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.header__menu'),
+    menuItem = document.querySelectorAll('.header__menu-item'),
+    hamburger = document.querySelector('.humburger__menu');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('humburger__menu__active');
+        menu.classList.toggle('header__menu__active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('humburger__menu__active');
+            menu.classList.toggle('menu__active');
+        })
+    })
+})
+
